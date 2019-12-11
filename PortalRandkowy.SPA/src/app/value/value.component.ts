feeb993ce5299e12bc42f1271d12ec0cf3cd762d
Lukasz Kwiatkowski <lukasz_kwiatek@wp.pl>
2019-12-11 18:27:52 +0100

    Wyswietlanie danych z API na stonie

--- PortalRandkowy.SPA/src/app/value/value.component.ts
+++ PortalRandkowy.SPA/src/app/value/value.component.ts
@@ -12,9 +12,9 @@ export class ValueComponent implements OnInit {
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.getvalues;
+    this.getValues();
   }
-  getvalues(){
+  getValues(){
     this.http.get('http://localhost:5000/api/values').subscribe(response => {
       this.values = response;
     }, error =>{
