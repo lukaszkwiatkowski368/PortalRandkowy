c38f6b8be9f044fadcf405f6f19b739a920e25ee
lukaszkwiatkowski368 <lukasz_kwiatek@wp.pl>
2019-12-19 14:35:03 +0100

    Integracja repozytorium z pracy

--- PortalRandkowy.SPA/src/app/value/value.component.ts
+++ PortalRandkowy.SPA/src/app/value/value.component.ts
@@ -7,6 +7,7 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./value.component.css']
 })
 export class ValueComponent implements OnInit {
+
   values: any;
 
   constructor(private http: HttpClient) { }
@@ -17,7 +18,7 @@ export class ValueComponent implements OnInit {
   getValues(){
     this.http.get('http://localhost:5000/api/values').subscribe(response => {
       this.values = response;
-    }, error =>{
+    }, error => {
       console.log(error);
     });
   }
