f55cceac3baf824f372389e5aca8b8743848343b
Lukasz Kwiatkowski <lukasz_kwiatek@wp.pl>
2020-01-02 20:14:12 +0100

    Dodajemy metode rej. użytk, wyświetlanie powiadomi

--- PortalRandkowy.SPA/src/app/register/register.component.ts
+++ PortalRandkowy.SPA/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -7,23 +9,24 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  @Input() valuesFromHome: any;
   @Output() cancelRegister = new EventEmitter();
 
   model: any = {};
 
-  constructor() { }
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
   register() {
-    console.log(this.model);
+    this.authService.register(this.model).subscribe(() => {
+      this.alertify.success('rejestracja udana');
+    }, error => {
+      this.alertify.error('wystąpił błąd rejestracji');
+    });
   }
-
   cancel() {
     this.cancelRegister.emit(false);
-    console.log('Anulowane');
   }
 
 }
